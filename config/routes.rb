d73c2b9464e15664d77715f25105e389905eef26
Steven Nunez <snunez@cyrusinnovation.com>
2013-06-04 00:11:24 -0400

    Finish Locking down app

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 ShirtManagementApp::Application.routes.draw do
+  get "home/index"
   devise_for :users, :path_names => { sign_in: 'login', sign_out: 'logout' }
   resources :categories
 
   resources :vendors
 
-  root "shirts#index"
+  root "home#index"
   get "/search", to: "shirts#search"
   resources :shirts
 end
