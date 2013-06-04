b6bdfcdbce94fbc3bb1682926ffd619ef89dec2e
Steven Nunez <snunez@cyrusinnovation.com>
2013-06-03 20:59:22 -0400

    Add Categories and tags

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
-  has_many :tags
+  has_many :tags, dependent: :destroy
   has_many :shirts, through: :tags
 end
--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -2,7 +2,8 @@ class Shirt < ActiveRecord::Base
   validates :name, :description, :image, presence: true
   validates :description, length: { minimum: 10 }
   validates :image, format: { with: /\A\w+.png\z/ }
-  has_many :tags
+
+  has_many :tags, dependent: :destroy
   has_many :categories, through: :tags
   belongs_to :vendor
 
@@ -11,20 +12,18 @@ class Shirt < ActiveRecord::Base
   end
 
   def tag_list
-    self.categories.pluck(:name)
+    self.categories.collect { |tag_name| tag_name.name }
   end
 
-   def tag_list=(tags)
-    tags = tags.split(/,\s*/)
-
-    tags.each do |tag|
-      unless self.categories.find_by name: tag
-        self.categories << Category.where(name: tag).first_or_create
+  def tag_list=(tags)
+    new_tags = tags.split(", ").each do |tag|
+      if Category.where(:name => tag).exists?
+        next
+      else
+        self.categories << Category.where(:name => tag).first_or_create
       end
     end
-    self.categories.where.not(name: tags).destroy_all
   end
-
 end
 
 
