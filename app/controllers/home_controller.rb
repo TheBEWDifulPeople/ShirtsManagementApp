8374d931c825b300e2c48516bd214e1b68298b8d
Steven Nunez <snunez@cyrusinnovation.com>
2013-06-04 20:59:56 -0400

    Prevent logged in users from seeing home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :reject_authenticated
 
   def index
+  end
+
+  private
+
+  def reject_authenticated
+    redirect_to shirts_path if current_user
   end
 end
